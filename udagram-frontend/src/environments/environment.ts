52e45ccc1703794e68d3fe47b11ee74d8a965dc2
Youssef Hilaly <yossefhilaly@gmail.com>
2022-09-19 00:58:36 +0200

    upload screenshots

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a82a00739a6c64b2ca60e25a6c7f5691-2024368269.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
